c7860019d73544cef05a8341def4fecd39e8e70e
Lars Hellemo <hellemo@gmail.com>
2020-07-18 10:41:18 +0200

    Simplifications and cleanup.

--- src/rk4.jl
+++ src/rk4.jl
@@ -14,59 +14,58 @@
 # Port from C to Julia by Lars Hellemo (May 2014)
 
 function checkBounds2d( point, Isize)
-    if (point[1]<1)||(point[2]<1)||(point[1]>(Isize[1]))||(point[2]>(Isize[2]))
+    if (point[1] < 1) || (point[2] < 1) || (point[1] > (Isize[1])) || (point[2] > (Isize[2]))
         return false
     end
     return true
 end
 
-function interpgrad2d(Ireturn::AbstractArray{T},I, Isize, point) where T
+function interpgrad2d!(Ireturn::AbstractArray{T},I, Isize, point) where T
     #  Linear interpolation variables
-
     perc = zeros(T,4,)
     index = fill(4,4,1)
     fTlocalx = floor(point[1])
     fTlocaly = floor(point[2])
     xBas0 = Int(fTlocalx)
     yBas0 = Int(fTlocaly)
-    xBas1=xBas0+1
-    yBas1=yBas0+1
+    xBas1 = xBas0 + 1
+    yBas1 = yBas0 + 1
 
     # Linear interpolation constants (percentages)
-    xCom=point[1]-fTlocalx
-    yCom=point[2]-fTlocaly
-    xComi=(1-xCom)
-    yComi=(1-yCom)
-    perc[1]=xComi * yComi
-    perc[2]=xComi * yCom
-    perc[3]=xCom * yComi
-    perc[4]=xCom * yCom
+    xCom = point[1] - fTlocalx
+    yCom = point[2] - fTlocaly
+    xComi = (1 - xCom)
+    yComi = (1 - yCom)
+    perc[1] = xComi * yComi
+    perc[2] = xComi * yCom
+    perc[3] = xCom * yComi
+    perc[4] = xCom * yCom
 
     # Stick to boundary
-    if xBas0<1
-        xBas0=1
-        if xBas1<1
-            xBas1=1
+    if xBas0 < 1
+        xBas0 = 1
+        if xBas1 < 1
+            xBas1 = 1
         end
     end
 
-    if yBas0<1
-        yBas0=1
-        if yBas1<1
-            yBas1=1
+    if yBas0 < 1
+        yBas0 = 1
+        if yBas1 < 1
+            yBas1 = 1
         end
     end
-    if xBas1>Isize[1]
-        xBas1=Isize[1]
-        if xBas0>Isize[1]
-            xBas0=Isize[1]
+    if xBas1 > Isize[1]
+        xBas1 = Isize[1]
+        if xBas0 > Isize[1]
+            xBas0 = Isize[1]
         end
     end
 
-    if yBas1>Isize[2]
-        yBas1=Isize[2]
-        if yBas0>Isize[2]
-            yBas0=Isize[2]
+    if yBas1 > Isize[2]
+        yBas1 = Isize[2]
+        if yBas0 > Isize[2]
+            yBas0 = Isize[2]
         end
     end
 
@@ -76,106 +75,71 @@ function interpgrad2d(Ireturn::AbstractArray{T},I, Isize, point) where T
     index[3] = LinearIndices(size(I))[xBas1,yBas0,1]
     index[4] = LinearIndices(size(I))[xBas1,yBas1,1]
 
-   f=Isize[1]*Isize[2]
+    f = Isize[1] * Isize[2]
 
     # the interpolated color
-    Ireturn[1]=I[index[1]]*perc[1]+I[index[2]]*perc[2]+I[index[3]]*perc[3]+I[index[4]]*perc[4]
-    Ireturn[2]=I[index[1]+f]*perc[1]+I[index[2]+f]*perc[2]+I[index[3]+f]*perc[3]+I[index[4]+f]*perc[4]
-
+    Ireturn[1] = sum(I[index[i]] * perc[i] for i=1:4)
+    Ireturn[2] = sum(I[index[i] + f] * perc[i] for i=1:4) 
     return Ireturn
 end
 
-function rk4(startPoint::AbstractArray{T}, GradientVolume::AbstractArray{T}, Stepsize::Number) where T
-
-    # Perform the RK4 raytracing step
-
-   gradientArray = GradientVolume # TODO Test if this works (without copy)
-    gradientArraySize = [size(gradientArray)...]
-    nextPoint = startPoint
-
-    startPoint1 = startPoint
-    nextPoint =  RK4STEP_2D(gradientArray, startPoint1, ones(T,2,), Stepsize)
-   return nextPoint
+"""
+    Perform the RK4 raytracing step
+"""
+function rk4(startpoint::AbstractArray{T}, gradientvolume::AbstractArray{T}, stepsize::Number) where T
+    gradientarray = gradientvolume # TODO Test if this works (without copy)
+    gradientarraysize = collect(size(gradientarray))
+    nextpoint = startpoint
+    nextPoint =  rk4step2d(gradientarray, startpoint, ones(T,2,), stepsize)
+    return nextPoint
 end
 
-
+"""
+Deprecated name for rk4step2d
+    """
 function RK4STEP_2D(gradientArray::AbstractArray{T}, startPoint::AbstractArray{T}, nextPoint,stepSize) where T
+    rk4step2d(gradientArray, startPoint, nextPoint, stepSize)
+end
+@deprecate RK4STEP_2D(gradientArray, startPoint, nextPoint, stepSize) rk4step2d(gradientarray, startpoint, nextpoint, stepsize)
+"""
+    Perform one step of the RK4 algorithm
+"""
+function rk4step2d(gradientarray::AbstractArray{T}, startpoint::AbstractArray{T}, nextpoint, stepsize) where T
+    k = ones(T,4,2)
+    temppoint = ones(T,2)
+    gradientarraysize = collect(size(gradientarray))
 
-    # Perform one step of the RK4 algorithm
-    k1 = ones(T,2,)
-    k2 = ones(T,2,)
-    k3 = ones(T,2,)
-    k4 = ones(T,2,)
-    tempPoint = ones(T,2,)
-
-    gradientArraySize = [size(gradientArray)...]
-
-    # Calculate k1
-    k1 = interpgrad2d(k1, gradientArray, gradientArraySize, startPoint)
-    tempnorm=max(norm(k1),1e-6)
-    k1[1] = k1[1]*stepSize/tempnorm
-    k1[2] = k1[2]*stepSize/tempnorm
-
-    tempPoint[1]=startPoint[1] - k1[1]*0.5
-    tempPoint[2]=startPoint[2] - k1[2]*0.5
-
-    # Check if still inside the domain
-    if !checkBounds2d([tempPoint...], gradientArraySize)
-        return ones(2,)
+    # Calculate k1:k4
+    for ki = 1:4
+        @views interpgrad2d!(k[ki,:], gradientarray, gradientarraysize, startpoint)
+        @views tempnorm = max(norm(k[ki,:]),1e-6)
+        for p = 1:2
+            k[ki,p] *= stepsize/tempnorm
+            temppoint[p] = startpoint[p] - k[ki,p] * 0.5
+        end
+        # Check if still inside the domain
+        if !checkBounds2d(temppoint, gradientarraysize)
+            return ones(2,)
+        end
     end
-
-    # Calculate k2
-    k2 = interpgrad2d(k2, gradientArray, gradientArraySize, tempPoint)
-    tempnorm=max(norm(k2),1e-6)
-    k2[1] = k2[1]*stepSize/tempnorm
-    k2[2] = k2[2]*stepSize/tempnorm
-
-    tempPoint[1]=startPoint[1] - k2[1]*0.5
-    tempPoint[2]=startPoint[2] - k2[2]*0.5
-
-    # Check the if are still inside the domain
-    if !checkBounds2d([tempPoint...], gradientArraySize)
-        return ones(2,)
-    end
-
-    # Calculate k3
-    k3 = interpgrad2d(k3, gradientArray, gradientArraySize, tempPoint)
-    tempnorm=max(norm(k3),1e-6)
-    k3[1] = k3[1]*stepSize/tempnorm
-    k3[2] = k3[2]*stepSize/tempnorm
-
-    tempPoint[1]=startPoint[1] - k3[1]
-    tempPoint[2]=startPoint[2] - k3[2]
-
-    # Check the if are still inside the domain
-    if !checkBounds2d([tempPoint...], gradientArraySize)
-        return ones(2,)
-    end
-
-    # Calculate k4
-    k4 = interpgrad2d(k4, gradientArray, gradientArraySize, tempPoint)
-    tempnorm=max(norm(k4),1e-6)
-    k4[1] = k4[1]*stepSize/tempnorm
-    k4[2] = k4[2]*stepSize/tempnorm
-
+    
     # Calculate final point
-    nextPoint[1] = startPoint[1] - (k1[1] + k2[1]*2.0 + k3[1]*2.0 + k4[1])/6.0
-    nextPoint[2] = startPoint[2] - (k1[2] + k2[2]*2.0 + k3[2]*2.0 + k4[2])/6.0
+    for p = 1:2
+        nextpoint[p] = startpoint[p] - (k[1,p] + k[2,p] * 2.0 + k[3,p] * 2.0 + k[4,p])/6.0
+    end
 
     # Set step to step size
-    # /*
-    # D[0]=(nextPoint[0]-startPoint[0]);
-    # D[1]=(nextPoint[1]-startPoint[1]);
-    # dl=stepSize/(sqrt(D[0]*D[0]+D[1]*D[1])+1e-15);
-    # D[0]*=dl; D[1]*=dl;
-    # nextPoint[0]=startPoint[0]+D[0];
-    # nextPoint[1]=startPoint[1]+D[1];
-    #*/
+    # D1 = nextPoint[1] - startPoint[1]
+    # D2 = nextPoint[2] - startPoint[2]
+    # dl = stepSize/(sqrt(D1 * D1 + D2 * D2)+1e-15)
+    # D1 *= dl
+    # D2 *= dl
+    # nextPoint[1] = startPoint[1] + D1
+    # nextPoint[2] = startPoint[2] + D2
 
     # Check the if are still inside the domain
-    if !checkBounds2d([nextPoint...], gradientArraySize)
+    if !checkBounds2d(nextpoint, gradientarraysize)
         return ones(2,)
     end
-
-    return nextPoint
+    return nextpoint
 end
